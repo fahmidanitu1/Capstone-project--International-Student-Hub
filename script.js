d6fc481b07e5d88d88f2b5365fa7261ba784d9d9
fahmi <shah2305341460@diu.edu.bd>
2026-02-12 21:04:39 +0600

    updated files

--- script.js
+++ script.js
@@ -79,4 +79,25 @@ if (document.getElementById("tipForm")) {
     });
 
     loadTips();
+    document.addEventListener("DOMContentLoaded", (event) => {
+
+
+  const menuBars = document.querySelector("#open-close--menu");
+  const navigation = document.querySelector(".header .navigation");
+  const closeMenuBars = document.querySelector("#close-menu--bars");
+
+
+
+    menuBars.addEventListener('click', (e) => {
+        e.preventDefault();
+        navigation.classList.toggle('open');
+    });
+
+    closeMenuBars.addEventListener("click", (e) => {
+        e.preventDefault();
+        navigation.classList.toggle('open');
+    });
+    
+
+});
 }
